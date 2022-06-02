6d28fb4850b02781b04858ece0bf26194c3b2d5a
Unekwe Francis <unekwenchekwube@gmail.com>
2022-06-02 12:55:56 +0100

    added view binding dependency

--- app/src/main/java/com/xisco/qrscanner/MainActivity.kt
+++ app/src/main/java/com/xisco/qrscanner/MainActivity.kt
@@ -2,10 +2,21 @@ package com.xisco.qrscanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.budiyev.android.codescanner.CodeScanner
 
+
+private const val  CAMERA_REQUEST_CODE = 14607
 class MainActivity : AppCompatActivity() {
+            private lateinit var codeScannerVar: CodeScanner
             override fun onCreate(savedInstanceState: Bundle?) {
                         super.onCreate(savedInstanceState)
                         setContentView(R.layout.activity_main)
+
+                        codeScannerFun()
+            }
+
+
+            private fun codeScannerFun (){
+    codeScannerVar = CodeScanner(this,)
             }
 }
