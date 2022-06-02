2e947e5e6d934b5f544e461e6804accaa580cbe5
Unekwe Francis <unekwenchekwube@gmail.com>
2022-06-02 20:00:48 +0100

    changed splashscreen and edited statusbar color

--- app/src/main/java/com/xisco/qrscanner/MainActivity.kt
+++ app/src/main/java/com/xisco/qrscanner/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
 
             override fun onCreate(savedInstanceState: Bundle?) {
                         super.onCreate(savedInstanceState)
-
+               setTheme(R.style.Theme_QRScanner)
                         binding = ActivityMainBinding.inflate(layoutInflater)
                         val view = binding.root
                         setContentView(view)
